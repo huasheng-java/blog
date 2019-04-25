91b28b54c8f568901343dd5b0fd0bb7b8ed4b94d
wangls <1845489946@qq.com>
2019-04-25 17:09:06 +0800

    初始化

--- src/main/java/com/luotf/controller/NavController.java
+++ src/main/java/com/luotf/controller/NavController.java
@@ -1,12 +1,10 @@
 package com.luotf.controller;
 
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
